1d4a63e28ebd79671b0f342375e4dd417fd7386b
Jeffrey Tan <jeffreytan@fb.com>
2024-11-03 14:47:53 -0800

    Reload fblldb module for reuse lldb-dap
    Summary:
    This diff fixes fblldb custom commands import issue during "reuse lldb-dap" feature.
    While reusing lldb-dap, we noticed that meta's internal commands such as fbpaste, or py_bt are not available in incremental debug sessions. This happens because `fblldb` module has been loaded in lldb's process address space during the first debug session, so the second debug session's import does not execute the module top level code which performs custom lldb commands registeration.
    
    To fix this issue, the diff reload the `fblldb` module which will re-register all internal commands.
    
    Long term, we should move everything command registration to lldb's lifetime event callback instead of relying on Python's module system to avoid this kind of issue.
    
    Test Plan: `fbpaste` command is available again.
    
    Reviewers: samkitono, toyang, alexandreperez, wanyi, #lldb_team
    
    Reviewed By: alexandreperez
    
    Subscribers: #lldb_team
    
    Differential Revision: https://phabricator.intern.facebook.com/D65401873

--- lldb/source/Interpreter/CommandInterpreter.cpp
+++ lldb/source/Interpreter/CommandInterpreter.cpp
@@ -2598,7 +2598,10 @@ void CommandInterpreter::SourceInitFileHome(CommandReturnObject &result,
   //
   // We don't have the fblldbinit module on windows, so we don't include it for
   // that build.
-  HandleCommand("script import fblldbinit", eLazyBoolNo, result);
+  HandleCommand("script import sys, importlib; "
+                "importlib.reload(sys.modules['fblldb']) if 'fblldb' "
+                "in sys.modules else __import__('fblldbinit')",
+                eLazyBoolNo, result);
 #endif
 
   llvm::SmallString<128> init_file;
