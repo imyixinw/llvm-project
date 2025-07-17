kevinfrei/llvm

Support reuse lldb-dap across VSCode debug sessions
Summary:
This diff adds a new mode in lldb-dap to support reusing lldb-dap across debug sessions which can improve incremental debug session launch/attach time.

The reuse lldb-dap design is implemented by two parts:
* Changes in VSCode fb-lldb extension to decide the reuse policy. Like how long to keep lldb-dap alive, when to reuse, when to spawn a separate instance (like support parallel debug sessions). D59239749 and D58840599
* A new `--keep-alive <timeout_in_ms>` option is added to lldb-dap to keep lldb-dap alive. This diff.

To support keep alive, a new `waitDataAvailableOrTimeout()` function is added in I/O code of lldb-dap to wait for next packet or specified timeout. And the DAP loop will wait for next packet with timeout after disconnection instead of exiting. Also, the global DAP object is reset to initial state.

Test framework is updated to support keep-alive mode and a new test case is added to demonstrate reuse across debug sessions.

Since this feature depends on two parts collaboration, the best way is first landing it as an internal patch for dogfooding (controlled by VSCode fb-lldb options) so that we can surface issues out of it. Once we feel confident about the feature, I will bring it to upstream.

Test Plan:
Test framework is updated to support keep-alive mode and a new test case is added to demonstrate reuse across debug sessions.

Measured performance improvement for incremental debug sessions:
```
adfinder built with ads focused mode:
* changed from 35s => 6s

//systemhealth/parrot:parrot_debug built with mode/lldb:
* 13GB of debug info and 199 total modules (3 modules hydrated with symbol on-demand)
* changed from 55s => 5s

//fblearner/feature_store/cpp:server built with mode/lldb:
* 58GB of debug info and 889 total modules (2 modules hydrated with symbol on-demand)
* changed from 110s => 11s

```

Reviewers: freik, gclayton, toyang, wanyi, #lldb_team

Reviewed By: freik

Subscribers: fr

lldb/packages/Python/lldbsuite/test/tools/lldb-dap/lldbdap_testcase.py
@@ -12,23 +12,40 @@ class DAPTestCaseBase(TestBase):
     # timeout by a factor of 10 if ASAN is enabled.
     timeoutval = 10 * (10 if ('ASAN_OPTIONS' in os.environ) else 1)
     NO_DEBUG_INFO_TESTCASE = True
+    # Caches last dap server for reusing.
+    alive_dap_server = None
 
-    def create_debug_adaptor(self, lldbDAPEnv=None):
+    def create_debug_adaptor(
+        self, lldbDAPEnv=None, keepAliveTimeout=None, reuseDapServer=False
+    ):
         """Create the Visual Studio Code debug adaptor"""
         self.assertTrue(
             is_exe(self.lldbDAPExec), "lldb-dap must exist and be executable"
         )
         log_file_path = self.getBuildArtifact("dap.txt")
+
+        if reuseDapServer:
+            self.assertIsNotNone(
+                DAPTestCaseBase.alive_dap_server, "No alive dap server found."
+            )
+            self.dap_server = DAPTestCaseBase.alive_dap_server
+            self.dap_server.reset(self.setUpCommands(), keepAlive=True)
+            return
+
         self.dap_server = dap_server.DebugAdaptorServer(
             executable=self.lldbDAPExec,
             init_commands=self.setUpCommands(),
             log_file=log_file_path,
             env=lldbDAPEnv,
+            keepAliveTimeout=keepAliveTimeout,
         )
+        if keepAliveTimeout is not None:
+            # Keep alive for future reuse
+            DAPTestCaseBase.alive_dap_server = self.dap_server
 
-    def build_and_create_debug_adaptor(self, lldbDAPEnv=None):
+    def build_and_create_debug_adaptor(self, lldbDAPEnv=None, keepAliveTimeout=None):
         self.build()
-        self.create_debug_adaptor(lldbDAPEnv)
+        self.create_debug_adaptor(lldbDAPEnv, keepAliveTimeout)
 
     def set_source_breakpoints(self, source_path, lines, data=None):
         """Sets source breakpoints and returns an array of strings containing
@@ -477,11 +494,12 @@ class DAPTestCaseBase(TestBase):
         customThreadFormat=None,
         launchCommands=None,
         expectFailure=False,
+        keepAliveTimeout=None,
     ):
         """Build the default Makefile target, create the DAP debug adaptor,
         and launch the process.
         """
-        self.build_and_create_debug_adaptor(lldbDAPEnv)
+        self.build_and_create_debug_adaptor(lldbDAPEnv, keepAliveTimeout)
         self.assertTrue(os.path.exists(program), "executable must exist")
 
         return self.launch(

lldb/test/API/tools/lldb-dap/reuseDAP/TestDAP_reuseAdapter.py
@@ -1,0 +1,39 @@
+"""
+Test reuse lldb-dap adapter across debug sessions.
+"""
+
+import dap_server
+from lldbsuite.test.decorators import *
+from lldbsuite.test.lldbtest import *
+from lldbsuite.test import lldbutil
+import lldbdap_testcase
+import time
+import os
+
+
+class TestDAP_reuseAdpater(lldbdap_testcase.DAPTestCaseBase):
+    @skipIfWindows
+    def test_basic_reuse(self):
+        """
+        Test reuse lldb-dap works across debug sessions.
+        """
+        program = self.getBuildArtifact("a.out")
+
+        # Keep lldb-dap alive for 10 minutes.
+        dapKeepAliveTimeInMS = 10 * 1000 * 60 
+        self.build_and_launch(program, disconnectAutomatically=False, keepAliveTimeout=dapKeepAliveTimeInMS)
+
+        source = "main.cpp"
+        breakpoint1_line = line_number(source, "// breakpoint 1")
+        breakpoint_ids = self.set_source_breakpoints(source, [breakpoint1_line])
+        self.continue_to_breakpoints(breakpoint_ids)
+        self.dap_server.request_disconnect()
+
+        # Second debug session by reusing lldb-dap.
+        self.create_debug_adaptor(reuseDapServer=True)
+        self.launch(program)
+        
+        breakpoint2_line = line_number(source, "// breakpoint 2")
+        breakpoint_ids = self.set_source_breakpoints(source, [breakpoint2_line])
+        self.continue_to_breakpoints(breakpoint_ids)
+        self.dap_server.request_disconnect()

imyixinw/llvm-project

lldb/packages/Python/lldbsuite/test/tools/lldb-dap/lldbdap_testcase.py
@@ -15,10 +15,14 @@ class DAPTestCaseBase(TestBase):
     # timeout by a factor of 10 if ASAN is enabled.
     DEFAULT_TIMEOUT = 10 * (10 if ("ASAN_OPTIONS" in os.environ) else 1)
     NO_DEBUG_INFO_TESTCASE = True
+    # Caches last dap server for reusing.
+    alive_dap_server = None
 
     def create_debug_adapter(
         self,
         lldbDAPEnv: Optional[dict[str, str]] = None,
+        keepAliveTimeout: Optional[int] = None,
+        reuseDapServer: bool = False,
         connection: Optional[str] = None,
     ):
         """Create the Visual Studio Code debug adapter"""
@@ -26,21 +30,35 @@ class DAPTestCaseBase(TestBase):
             is_exe(self.lldbDAPExec), "lldb-dap must exist and be executable"
         )
         log_file_path = self.getBuildArtifact("dap.txt")
+
+        if reuseDapServer:
+            self.assertIsNotNone(
+                DAPTestCaseBase.alive_dap_server, "No alive dap server found."
+            )
+            self.dap_server = DAPTestCaseBase.alive_dap_server
+            self.dap_server.reset(self.setUpCommands(), keepAlive=True)
+            return
+
         self.dap_server = dap_server.DebugAdapterServer(
             executable=self.lldbDAPExec,
             connection=connection,
             init_commands=self.setUpCommands(),
             log_file=log_file_path,
             env=lldbDAPEnv,
+            keepAliveTimeout=keepAliveTimeout,
         )
+        if keepAliveTimeout is not None:
+            # Keep alive for future reuse
+            DAPTestCaseBase.alive_dap_server = self.dap_server
 
     def build_and_create_debug_adapter(
         self,
         lldbDAPEnv: Optional[dict[str, str]] = None,
+        keepAliveTimeout: Optional[int] = None,
         dictionary: Optional[dict] = None,
     ):
         self.build(dictionary=dictionary)
-        self.create_debug_adapter(lldbDAPEnv)
+        self.create_debug_adapter(lldbDAPEnv, keepAliveTimeout)
 
     def build_and_create_debug_adapter_for_attach(self):
         """Variant of build_and_create_debug_adapter that builds a uniquely
@@ -483,12 +501,13 @@ class DAPTestCaseBase(TestBase):
         program,
         *,
         lldbDAPEnv: Optional[dict[str, str]] = None,
+        keepAliveTimeout: Optional[int] = None,
         **kwargs,
     ):
         """Build the default Makefile target, create the DAP debug adapter,
         and launch the process.
         """
-        self.build_and_create_debug_adapter(lldbDAPEnv)
+        self.build_and_create_debug_adapter(lldbDAPEnv, keepAliveTimeout)
         self.assertTrue(os.path.exists(program), "executable must exist")
 
         return self.launch(program, **kwargs)

lldb/test/API/tools/lldb-dap/reuseDAP/TestDAP_reuseAdapter.py
@@ -1,0 +1,39 @@
+"""
+Test reuse lldb-dap adapter across debug sessions.
+"""
+
+import dap_server
+from lldbsuite.test.decorators import *
+from lldbsuite.test.lldbtest import *
+from lldbsuite.test import lldbutil
+import lldbdap_testcase
+import time
+import os
+
+
+class TestDAP_reuseAdpater(lldbdap_testcase.DAPTestCaseBase):
+    @skipIfWindows
+    def test_basic_reuse(self):
+        """
+        Test reuse lldb-dap works across debug sessions.
+        """
+        program = self.getBuildArtifact("a.out")
+
+        # Keep lldb-dap alive for 10 minutes.
+        dapKeepAliveTimeInMS = 10 * 1000 * 60 
+        self.build_and_launch(program, disconnectAutomatically=False, keepAliveTimeout=dapKeepAliveTimeInMS)
+
+        source = "main.cpp"
+        breakpoint1_line = line_number(source, "// breakpoint 1")
+        breakpoint_ids = self.set_source_breakpoints(source, [breakpoint1_line])
+        self.continue_to_breakpoints(breakpoint_ids)
+        self.dap_server.request_disconnect()
+
+        # Second debug session by reusing lldb-dap.
+        self.create_debug_adapter(reuseDapServer=True)
+        self.launch(program)
+        
+        breakpoint2_line = line_number(source, "// breakpoint 2")
+        breakpoint_ids = self.set_source_breakpoints(source, [breakpoint2_line])
+        self.continue_to_breakpoints(breakpoint_ids)
+        self.dap_server.request_disconnect()